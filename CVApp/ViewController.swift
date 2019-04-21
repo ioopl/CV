b26312b1cd3f9c9bf73004bc82a6d2a8e4a4c5de
Umair Hasan <umair.hasan@ocado.com>
2019-04-21 22:42:24 +0100

    MVC pattern with RxSwift proof of concept work done for fetching remote JSON - WIP

--- CVApp/ViewController.swift
+++ CVApp/ViewController.swift
@@ -10,13 +10,91 @@ import RxSwift
 import RxCocoa
 import SnapKit
 
+struct ResumeResult: Codable {
+    let contents: [ResumeDto]
+}
+
+struct ResumeDto: Codable, Equatable {
+    let title: String
+}
+
+enum ResumeRestClientError: Error {
+    case error
+}
+
+enum Resume: String {
+    case title
+    case href
+    case description
+    case thumbnail
+}
+
+enum TableViewCell: String {
+    case cell = "Cell"
+}
+
+
+class ResumeRestClient {
+    func getCV() -> Observable<[ResumeDto]> {
+        let urlComponents = URLComponents(url: URL(string: "https://api.myjson.com/bins/vsgac")!, resolvingAgainstBaseURL: true)!
+        let request = URLRequest(url: urlComponents.url!)
+        return RemoteServiceDispatcher().dispatch(request: request).flatMap({ (response: HTTPURLResponse, data: Data) -> Observable<[ResumeDto]> in
+
+            if 200..<300 ~= response.statusCode {
+                let decoder = JSONDecoder()
+                let result = try! decoder.decode(ResumeResult.self, from: data)
+                return Observable.just(result.contents)
+            } else {
+                return Observable.error(ResumeRestClientError.error)
+            }
+        })
+    }
+}
+
+protocol RequestDispatcher {
+    func dispatch(request: URLRequest) -> Observable<(response: HTTPURLResponse, data: Data)>
+}
+
+class RemoteServiceDispatcher: RequestDispatcher {
+    func dispatch(request: URLRequest) -> Observable<(response: HTTPURLResponse, data: Data)> {
+        return URLSession.shared.rx.response(request: request)
+    }
+}
+
 class ViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+    private let tableView = UITableView(frame: CGRect.zero, style: .grouped)
+    private let searchBar = UISearchBar()
+    private let errorLabel = UILabel()
+    private let loading = UIActivityIndicatorView(style: .gray)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: TableViewCell.cell.rawValue)
+        view.addSubview(tableView)
+
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+
+        view.addSubview(loading)
+        loading.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
+
+        view.addSubview(errorLabel)
+        errorLabel.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
+
+        Observable.just([ResumeDto(title: Resume.title.rawValue)]).flatMapLatest { _ in
+                    ResumeRestClient().getCV()
+                }.bind(to: tableView.rx.items(cellIdentifier: TableViewCell.cell.rawValue)) { (index: Int, dto: ResumeDto, cell: UITableViewCell) in
+                    cell.textLabel?.text = dto.title
+                }.disposed(by: disposeBag)
     }
-
-
 }
 
