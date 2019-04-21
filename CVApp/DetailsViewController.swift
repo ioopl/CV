bbbea33adc8ea9619b24872066b2b6d524102ff0
Umair Hasan <umair.hasan@ocado.com>
2019-04-21 19:55:58 +0100

    Added Details View Controller to display information about cell selected.

---
+++ CVApp/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  CVApp
+//
+//  Created by Umair Hasan on 21/04/2019.
+//  Copyright © 2019 Umair Hasan. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
