a2b1abffe027595007f7c6f90d04674ad2d7111d
Umair Hasan <umair.hasan@ocado.com>
2019-04-22 13:02:50 +0100

    Added Details View controller logic which takes DTO in initialiser, also added a label to display the data.

--- CVApp/DetailsViewController.swift
+++ CVApp/DetailsViewController.swift
@@ -7,24 +7,32 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class DetailsViewController: UIViewController {
+
+    let disposeBag = DisposeBag()
+    let dto: ResumeDto
+    init(dto: ResumeDto) {
+        self.dto = dto
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        let label = UILabel()
+        view.addSubview(label)
+        label.text = dto.title
+        label.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
